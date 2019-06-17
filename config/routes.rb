174c3737df58d3b682d2181cac937dad24453a13
romy koenig <romyg.koenig@gmail.com>
2019-06-17 11:30:12 +0300

    added task model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'list', to: 'tasks#list', as: :list
 end
